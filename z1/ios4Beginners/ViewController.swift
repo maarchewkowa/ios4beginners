390182fc4811a77314d3785f3de59bfc5bb66cb2
Karolina Guzewska <k.guzewska@gmail.com>
2017-10-31 21:07:52 +0100

    Home Assignment #1

---
+++ z1/ios4Beginners/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ios4Beginners
+//
+//  Created by Karolina Beata Natalia Guzewska on 30.10.2017.
+//  Copyright © 2017 Karolina Beata Natalia Guzewska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func okButton(_ sender: Any) {
+        if (textField.text?.isEmpty)!
+        {
+            print("No text found!")
+        } else {
+        print(textField.text!)
+    }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
